b24107027216e8dff546440876e0c114e6afa382
Chris Achard <chrisachard@gmail.com>
2018-07-15 14:19:00 -0400

    Converts binary class model into multi class model, in order to train on the iris dataset

---
+++ multi_class_model.py
@@ -0,0 +1,43 @@
+from keras.models import Sequential
+from keras.layers import Dense, Activation
+from keras.utils.np_utils import to_categorical
+
+import numpy as np
+
+model = Sequential()
+
+
+# 0 = Iris-setosa
+# 1 = Iris-versicolor
+# 2 = Iris-virginica
+
+model.add(Dense(8, activation='relu', input_dim=4))
+model.add(Dense(16, activation='relu'))
+model.add(Dense(32, activation='relu'))
+model.add(Dense(64, activation='relu'))
+model.add(Dense(32, activation='relu'))
+model.add(Dense(16, activation='relu'))
+model.add(Dense(8, activation='relu'))
+model.add(Dense(3, activation='softmax'))
+
+model.compile(
+  optimizer='adam', 
+  loss='categorical_crossentropy',
+  metrics=['accuracy']
+)
+
+data = np.genfromtxt('iris.csv', delimiter=',')
+
+x_train = data[1:, :4]
+y_train = to_categorical(data[1:, 4])
+
+perm = np.random.permutation(y_train.shape[0])
+x_train = x_train[perm]
+y_train = y_train[perm]
+
+model.fit(
+  x_train, 
+  y_train,
+  epochs=100, 
+  validation_split=0.2
+)
