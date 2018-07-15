9615318c31c0d500e1c6377f1f6c6034bc36bbfa
Chris Achard <chrisachard@gmail.com>
2018-07-15 14:44:33 -0400

    Change the learning rate in the middle of training - and keep training, then save the model

---
+++ change_lr_and_keep_training.py
@@ -0,0 +1,57 @@
+import numpy as np
+
+from keras.models import Sequential
+from keras.layers import Dense, Activation
+from keras.utils.np_utils import to_categorical
+from keras.optimizers import Adam
+
+model = Sequential()
+
+model.add(Dense(8, activation='relu', input_dim=4))
+model.add(Dense(16, activation='relu'))
+model.add(Dense(32, activation='relu'))
+model.add(Dense(64, activation='relu'))
+model.add(Dense(32, activation='relu'))
+model.add(Dense(16, activation='relu'))
+model.add(Dense(8, activation='relu'))
+model.add(Dense(3, activation='softmax'))
+
+
+opt = Adam(lr=0.001)
+
+model.compile(
+  optimizer=opt, 
+  loss='categorical_crossentropy',
+  metrics=['accuracy']
+)
+
+# 0 = Iris-setosa
+# 1 = Iris-versicolor
+# 2 = Iris-virginica
+
+data = np.genfromtxt('iris.csv', delimiter=',')
+
+x_train = data[1:, :4]
+y_train = to_categorical(data[1:, 4])
+
+perm = np.random.permutation(y_train.shape[0])
+x_train = x_train[perm]
+y_train = y_train[perm]
+
+model.fit(
+  x_train, 
+  y_train,
+  epochs=100, 
+  validation_split=0.2
+)
+
+model.optimizer = Adam(lr=0.0001)
+
+model.fit(
+  x_train, 
+  y_train,
+  epochs=100, 
+  validation_split=0.2
+)
+
+model.save('iris.h5')
